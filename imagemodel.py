db8c2d872f6cffa9ad76d8f66c9b750f0b5ef0a7
akaza21 <pragyash20_ug@ece.nits.ac.in>
2024-06-06 18:30:57 +0530

    first commit

---
+++ imagemodel.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, request, render_template
+import os
+import validators
+import requests
+import replicate
+from io import BytesIO
+
+os.environ['REPLICATE_API_TOKEN'] = 'r8_KUkKYhfaYRChrpxVuhtguoPvJ5Zlncj0o6R75'
+
+app = Flask(__name__)
+app.static_folder = 'templates/static'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    image_url = request.form.get('image_url')
+    question = 'caption the image'
+
+    if not validate_image_url(image_url):
+        return jsonify({'error': 'Invalid image URL.'}), 400
+
+    if not is_valid_image(image_url):
+        return jsonify({'error': 'The URL does not point to a valid image.'}), 400
+
+    try:
+        response = requests.get(image_url, stream=True)
+        response.raise_for_status()  # Check for any request errors
+
+        image_bytes = BytesIO(response.content)  # Read the response content into BytesIO object
+
+        input_data = {"image": image_bytes, "question": question} if question else {"image": image_bytes}
+
+        output = replicate.run(
+            "andreasjansson/blip-2:4b32258c42e9efd4288bb9910bc532a69727f9acd26aa08e175713a0a857a608",
+            input=input_data
+        )
+
+        return jsonify({'caption': output})
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return jsonify({'error': 'An internal server error occurred.'}), 500
+
+
+
+def validate_image_url(image_url):
+    if not validators.url(image_url):
+        return False
+    return True
+
+def is_valid_image(image_url):
+    response = requests.head(image_url)
+    content_type = response.headers.get('content-type')
+    if content_type and 'image' in content_type:
+        return True
+    return False
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=9000, debug=True)
---
+++ templates/static/script.js
@@ -0,0 +1,78 @@
+function previewImage() {
+  var imagePreview = document.getElementById("image_preview");
+  var imageUrl = document.getElementById("image_url").value;
+  if (!validateImageUrl(imageUrl)) {
+    imageUrl = defaultImageUrl;
+  }
+
+  imagePreview.src = imageUrl;
+}
+
+
+function generateCaption() {
+  var imageUrl = document.getElementById("image_url").value;
+  // var question = document.getElementById("question").value;
+  var captionDiv = document.getElementById("caption_div");
+
+  // Reset previous error message
+  captionDiv.innerHTML = "";
+  captionDiv.classList.remove("error");
+
+  // Perform image URL validation
+  if (!validateImageUrl(imageUrl)) {
+    captionDiv.innerHTML = "Oops 😥! That looks like an invalid image URL. Supported images are .jpg, .jpeg, and .png.";
+    captionDiv.classList.add("error");
+    captionDiv.classList.remove("generated"); // Remove generated state class
+    return;
+  }
+
+  // Proceed with generating caption
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/generate");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  var data = "image_url=" + encodeURIComponent(imageUrl);
+  // if (question) {
+  //   data += "&question=" + encodeURIComponent(question);
+  // }
+
+  captionDiv.innerHTML = "Generating caption...";
+  captionDiv.classList.add("loading");
+  captionDiv.classList.remove("generated"); // Remove generated state class
+
+  xhr.onreadystatechange = function() {
+    if (this.readyState === XMLHttpRequest.DONE) {
+      if (this.status === 200) {
+        var response = JSON.parse(this.responseText);
+        if (response && response.caption) {
+          captionDiv.innerHTML = response.caption;
+          captionDiv.classList.add("generated"); // Apply generated state class
+          captionDiv.classList.remove("error"); // Remove error state class
+        } else {
+          captionDiv.innerHTML = "Failed to generate caption.";
+          captionDiv.classList.remove("generated"); // Remove generated state class
+          captionDiv.classList.add("error"); // Apply error state class
+        }
+      } else {
+        captionDiv.innerHTML = "Error generating caption.";
+        captionDiv.classList.remove("generated"); // Remove generated state class
+        captionDiv.classList.add("error"); // Apply error state class
+      }
+      captionDiv.classList.remove("loading");
+    }
+  }
+
+  xhr.send(data);
+}
+
+
+function validateImageUrl(imageUrl) {
+  // Perform image URL validation logic
+  // Customize this regular expression based on your specific requirements
+  var imageExtensions = /\.(jpeg|jpg|png|gif)(\?|$)/i;
+  return imageExtensions.test(imageUrl);
+}
+
+window.addEventListener("load", function() {
+  previewImage();
+});
